401d2fb4419c027113df6019a2ba0756caf86088
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2024-06-24 01:31:12 +0530

    initial commit 24-06-24

---
+++ contracts/NFTMarketplace.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+//internal import for net openzwlin 
+ import "@openezeppelin/contracts/utils/Counters.sol";
+ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
---
+++ scripts/deploy.js
@@ -0,0 +1,25 @@
+
+const hre = require("hardhat");
+
+async function main() {
+  
+
+  const lockedAmount = hre.ethers.parseEther("0.001");
+
+  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
+  });
+
+  await lock.waitForDeployment();
+
+  console.log(
+    `Lock with ${ethers.formatEther(
+      lockedAmount
+    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+  );
+}
+
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
