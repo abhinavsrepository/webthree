683db20747c51ed7a7ebb7002ef36288ea34f3d9
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2024-07-04 01:07:29 +0530

    initial commit

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -1,6 +1,37 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.26;
 
 //internal import for net openzwlin 
- import "@openezeppelin/contracts/utils/Counters.sol";
+ import "@openzeppelin/contracts/utils/Counters.sol";
+ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage";
  import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+ import "hardhat/console.sol";
+
+ contract NFTMarketplace is ERC721URIStorage{
+    using Counters for Counter.Counter;
+
+    Counters.Counter private _tokenIds;
+    Counters.Counter private_itemsSold;
+    address payable owner ;
+
+    mapping (uint256 => MarketItem ) private idMarketItem;
+
+    struct MarketItem {
+        uint256 tokeId;
+        address payable seller;
+        address payable owner;
+        uint256 price;
+        bool sold;
+
+    }
+    event idMarketItemCreated(
+        uint256 indexed tokenId,
+        address seller,
+        address owner,
+        uint256 price,
+        bool sold,
+
+    );
+    constructor() ERC721("NFT Metaverse Token","MYNFT")
+
+ }
