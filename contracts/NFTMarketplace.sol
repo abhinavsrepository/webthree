9b53bbc53fc834e694d791548334425e54a9de11
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2024-07-05 01:09:43 +0530

    hello

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -12,6 +12,8 @@ pragma solidity ^0.8.4;
 
     Counters.Counter private _tokenIds;
     Counters.Counter private_itemsSold;
+
+    uint256 listingPrice = 0.0015 ether;
     address payable owner ;
 
     mapping (uint256 => MarketItem ) private idMarketItem;
@@ -29,14 +31,45 @@ pragma solidity ^0.8.4;
         address seller,
         address owner,
         uint256 price,
-        bool sold,
+        bool sold
 
     );
+    modifier onlyOwner{
+        require(msg.sender == owner, "only owner of the marketplace can change the listing price ")
+    }
     constructor() ERC721("NFT Metaverse Token","MYNFT"){
         owner == payable(msg.sender);
 
     }
-    function updateListingPrice(
-         
-    )
+    function updateListingPrice(uint256 _ListingPrice) public payable onlyOwner{
+        listingPrice = _listingPrice;
+    }
+    function getListingPrice() public view returns(uint256){
+        return listingPrice;
+    }
+    // Let create "Create NFT token FUNCTION"
+    function createToken(string memory tokenURI,uint256 price ) public payable return (uint256){
+        _tokenIds.increment();
+        uint256 newTokenId = _tokenIds.current();
+    
+    _mint(msg.sender ,newTokenId);
+    
+    _setTokenURI(newTokenId,tokenURI);
+
+    createMarketItem(newTokenId,price);
+    return newTokenId;
+    }
+    // creating market items
+    function createMarketItem( uint256 tokenId,uint256 price)private{
+        require(price>0 , "Price must be atleast 1");
+        require(msg.value == listingPrice,"Price must be equal to lis" );
+        idMarketItem[tokenId] =MarketItem(
+            tokenId,
+            payable(msg.sender),
+            payable(address(this)),
+            price, 
+            false,
+
+        )
+    }
  }
